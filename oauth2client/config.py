eba5ee9a58d2c213d9bcae0525468f3098bf7520
Davide Vaghetti <davide.vaghetti@gmail.com>
2017-06-17 19:42:44 +0200

    OAuth 2.0 Simple Client

---
+++ oauth2client/config.py
@@ -0,0 +1,32 @@
+# DEBUG
+DEBUG = True
+TRAP_BAD_REQUEST_ERRORS = True
+
+# SECRET_KEY
+## Insert here your secret key
+## To generate a secret key in a python shell:
+## >>> import os
+## >>> os.urandom(24)
+SECRET_KEY = ''
+
+# OAuth2 Authentication Server
+## EXAMPLE: Github Authentication Server endpoints
+#AS_AUTH_URL = 'https://github.com/login/oauth/authorize'
+#AS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
+AS_AUTH_URL = ''
+AS_TOKEN_URL = ''
+
+
+# OAuth2 Resource Server
+## EXAMPLE: Github Resource Server API endpoint
+#RS_API_URL = 'https://api.github.com/'
+
+RS_API_URL = ''
+
+# OAuth2 Client
+CLIENT_ID  = ''
+CLIENT_SECRET = ''
+REDIRECT_URI = 'YOUR_BASE_URL+:9000/cb'
+SCOPE = 'user'
+
